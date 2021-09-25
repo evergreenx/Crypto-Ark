766b4238da0439d7188b5018c45f958ad41b7bae
ido dickson evergreen <dicksonevergreenido@gmail.com>
2021-09-26 00:23:37 +0100

    added needed pachages

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import { Switch, Route, Redirect } from 'react-router-dom';
-import Sidebar from 'components/Sidebar';
-import Home from 'pages/Home';
-import Cryptocurrencies from 'pages/Cryptocurrencies';
-import Exchanges from 'pages/Exchanges';
-import News from 'pages/News';
-import Footer from 'components/Footer';
-import Profile from 'pages/Profile'
+import { Switch, Route, Redirect } from "react-router-dom";
+import Sidebar from "components/Sidebar";
+import Home from "pages/Home";
+import Cryptocurrencies from "pages/Cryptocurrencies";
+import CrytoDetails from "pages/CrytoDetails";
+
+import Exchanges from "pages/Exchanges";
+import News from "pages/News";
+import Footer from "components/Footer";
+import Profile from "pages/Profile";
 
 // Tailwind CSS Style Sheet
-import 'assets/styles/tailwind.css';
+import "assets/styles/tailwind.css";
 
 function App() {
-    return (
-        <>
-            <Sidebar />
-            <div className="md:ml-64">
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/cryptocurrencies" component={Cryptocurrencies} />
-                    <Route exact path="/exchanges" component={Exchanges} />
-                    <Route exact path="/news" component={News} />
-                    <Route exact path="/profile" component={Profile} />
+  return (
+    <>
+      <Sidebar />
+      <div className="md:ml-64">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/cryptocurrencies" component={Cryptocurrencies} />
 
-                    <Redirect from="*" to="/" />
-                </Switch>
-                <Footer />
-            </div>
-        </>
-    );
+          <Route exact path="/crytoDetails" component={CrytoDetails} />
+
+          <Route exact path="/exchanges" component={Exchanges} />
+          <Route exact path="/news" component={News} />
+          <Route exact path="/profile" component={Profile} />
+
+          <Redirect from="*" to="/" />
+        </Switch>
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,13 @@ export default function Dashboard() {
             {/* <div className="bg-light-blue-500 px-3 md:px-8 h-40" /> */}
 
 
+            <div className="container">
+
+
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem fugit repellat quis tenetur incidunt, veniam consequatur saepe perspiciatis, repellendus necessitatibus itaque aliquam quos vero, ea quia magni pariatur reprehenderit excepturi.
+            </div>
+
+
             
 
           
