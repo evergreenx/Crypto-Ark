0f5cf28f0da96869a31dccf420723badb1135216
ido dickson evergreen <dicksonevergreenido@gmail.com>
2021-10-17 19:40:25 +0100

    fixing crypto details page

--- src/pages/CryptoDetails.tsx
+++ src/pages/CryptoDetails.tsx
@@ -1,50 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import HTMLReactParser from "html-react-parser";
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom";
 // import millify from 'millify';
-import {useGetCryptoDetailsQuery} from  '../services/cryptoApi'
-
-
-
+import { useGetCryptoDetailsQuery } from "../services/cryptoApi";
 
 const CryptoDetails = () => {
+  const { coinId } = useParams();
 
+  // console.log(coinId)
 
-  const {coinId} = useParams()
+  const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y"];
+  const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
+  const [timePeriod, setTimePeriod] = useState("7d");
 
-// console.log(coinId)
+  const [cryptoDetail, setCryptoDetail] = useState(data?.data?.coin);
 
-  const {data , isFetching} = useGetCryptoDetailsQuery(coinId);
+  console.log(data);
 
-  console.log(data)
-
-  if(isFetching) {
-
-    return <h2 className="text-4xl animate-pulse font-bold flex justify-center mt-20 text-orange-500">Loading</h2>;
-
+  if (isFetching) {
+    return (
+      <h2 className="text-4xl animate-pulse font-bold flex justify-center mt-20 text-orange-500">
+        Loading
+      </h2>
+    );
   }
 
-  return(
-<div className="py-8">
+  return (
+    <div className="py-8">
+      <div className="top-content mb-10 text-center">
+        <h1 className="font-bold text-3xl ">
+          {" "}
+          {cryptoDetail?.name} ({cryptoDetail?.slug})
+        </h1>
+
+        <p className="text-lg">
+          {cryptoDetail.name} live pirce is US dollars. View value statstics ,
+          market cap and supply
+        </p>
+      </div>
+
+      <div className="select-period w-36 mb-10">
+        <label className="font-bold">Select time period </label>
+        <select
+          onChange={(e) => setTimePeriod(e.target.value)}
+          className="block w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Select time period"
+        >
 
 
-<h1 className="font-bold text-2xl"> {data.data.coin.name}
+          <option selected="selected"> </option>
+          {time.map((i) => (
+            <option> {i} </option>
+          ))}
 
-  </h1>
+        </select>
+      </div>
 
-
-
-<h2 className="font-normal text-lg">
-
-  What Is {data.data.coin.name}
-{HTMLReactParser(data.data.coin.description)}
-
-  </h2>
-
-
-</div>
-
-  ) 
+      <h2 className="font-normal text-lg">
+        What Is {cryptoDetail?.name}
+        {HTMLReactParser(cryptoDetail?.description)}
+      </h2>
+    </div>
+  );
 };
 
 export default CryptoDetails;
